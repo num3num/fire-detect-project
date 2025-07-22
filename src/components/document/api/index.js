9f734dfa5172e02647cd7242527c7096dd1714ff
3num <63039592+num3num@users.noreply.github.com>
2025-07-22 15:55:35 +0800

    Delete src directory

--- src/components/document/api/index.js
+++
@@ -1,8 +0,0 @@
-import request from '../../../utils/request'
-
-export const docUpload = (file) => {
-    return request({
-        method:'post',
-        url:`/v1/detect?file_path=${file.file_path}&sava_path=${file.sava_path}`
-    } )
-}
--- src/components/document/fs.js
+++
@@ -1,5 +0,0 @@
- import fs from "fs"
-//  const fs = require('fs');
-    const base64 = fs.readFileSync('C:/Users/admin/Desktop/1.png').toString('base64');
-  export   const imgSrc = `data:image/png;base64,${base64}`
-    // console.log(`data:image/png;base64,${base64}`);
